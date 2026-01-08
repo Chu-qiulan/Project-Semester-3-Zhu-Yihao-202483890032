e00081a3b79f4f693b40491b6e792ed873313011
Chu-qiulan <1606464637@qq.com>
2026-01-08 20:20:27 +0800

    Add files via upload

---
+++ soilsensor.py
@@ -0,0 +1,18 @@
+import RP1.GPIO as GPIO
+import time
+
+channel = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel,GPIO.IN)
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("water detected")
+    else:
+        print("water detected")
+
+GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300)
+GPIO.add_event_callback(channel,callback)
+
+while True:
+        time.sleep(0)
